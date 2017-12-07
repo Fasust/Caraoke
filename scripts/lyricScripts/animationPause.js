3d011eb91d8790d75c0535a73242641d90cafa99
Sebastian Faust <sebastianf333@googlemail.com>
2017-12-07 16:12:40 +0100

    Play and Pause Button Script added

--- scripts/lyricScripts/animationPause.js
+++ scripts/lyricScripts/animationPause.js
@@ -4,11 +4,21 @@ var playing = 0;
 function activateLyricAninmation() {
 
     if(playing){
+        //Animation
         document.getElementById("lyric-text").style.animationPlayState = "paused";
+
+        //Switch Icons
+        document.getElementById("playButton").classList.add('glyphicon-play');
+        document.getElementById("playButton").classList.remove('glyphicon-pause');
         playing = 0;
     }else{
+        //Animation
         document.getElementById("lyric-text").style.animationPlayState = "running";
         document.getElementById("lyric-text").classList.toggle('lyric-text-active');
+
+        //Switch Icons
+        document.getElementById("playButton").classList.remove('glyphicon-play');
+        document.getElementById("playButton").classList.add('glyphicon-pause');
         playing = 1;
     }
 
