c89d9da7a12fa25f43c0d0d523dedb66bd13baf3
angart <tissen.arthur@gmail.com>
2017-12-07 16:16:56 +0100

    musicplayer progress bar animation

--- scripts/lyricScripts/animationPause.js
+++ scripts/lyricScripts/animationPause.js
@@ -10,7 +10,22 @@ function activateLyricAninmation() {
         document.getElementById("lyric-text").style.animationPlayState = "running";
         document.getElementById("lyric-text").classList.toggle('lyric-text-active');
         playing = 1;
+        progBarMove();
     }
 
 
+}
+
+function progBarMove() {
+    var progBar = document.getElementById("progBar");
+    var width = 1;
+    var id = setInterval(partialFrame, 150);
+    function partialFrame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width+=0.1;
+            progBar.style.width = width + '%';
+        }
+    }
 }
